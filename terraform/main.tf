5d4285bc68b14d45f026d161b9e44f114acaaa1e
sigier <jotulw@proton.me>
2025-04-04 01:16:29 +0300

    fix(ci): cloudfront distribution id secret added

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,17 +46,14 @@ resource "aws_cloudfront_distribution" "frontend" {
   enabled             = true
   default_root_object = "index.html"
 
-  origins {
-    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
-    origin_id   = "S3Origin"
+origin {
+  domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+  origin_id   = "S3Origin"
 
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
+  s3_origin_config {
+    origin_access_identity = "" 
   }
+}
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS Region"  
+  type        = string
+}
+
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
